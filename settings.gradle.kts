3d7661c472f05d50bf3207956087749c5bf9b31d
lana <lana.todorovski@gmail.com>
2024-05-10 08:21:28 +0200

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "android-projekt-g3p"
+include(":app")
+ 
