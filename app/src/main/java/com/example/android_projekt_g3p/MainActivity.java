f72e8c436b322031b90317c0b045ce09ac1f48c3
lanatodorovski <116957819+lanatodorovski@users.noreply.github.com>
2024-05-16 12:36:05 +0200

    promjene

--- app/src/main/java/com/example/android_projekt_g3p/MainActivity.java
+++ app/src/main/java/com/example/android_projekt_g3p/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.android_projekt_g3p;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.time.LocalDate;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+    private LocalDate selectedDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        monthYearText = findViewById(R.id.textView);
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        selectedDate = LocalDate.now();
     }
 }
