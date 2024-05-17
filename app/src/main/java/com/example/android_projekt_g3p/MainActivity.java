89d74897b40ce5a0b8e6e7af65d32c2018da8571
lanatodorovski <116957819+lanatodorovski@users.noreply.github.com>
2024-05-17 12:37:06 +0200

    date promjene opet

--- app/src/main/java/com/example/android_projekt_g3p/MainActivity.java
+++ app/src/main/java/com/example/android_projekt_g3p/MainActivity.java
@@ -6,8 +6,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalField;
+import java.time.temporal.WeekFields;
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +30,24 @@ public class MainActivity extends AppCompatActivity {
         selectedDate = LocalDate.now();
 
         monthYearText.setText(monthYearFromDate(selectedDate));
+        //ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+        daysInMonthArray(selectedDate);
     }
+    private void daysInMonthArray(LocalDate date){
+        ArrayList<String> daysInMonthArray = new ArrayList<>();
+        YearMonth yearMonth = YearMonth.from(date);
+
+        int daysInMonth = yearMonth.lengthOfMonth();
+
+
+        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
+        TemporalField fieldISO = WeekFields.of(Locale.UK).dayOfWeek();
+        LocalDate lastMondayOfMonthBefore = selectedDate.withDayOfMonth(1).with(fieldISO,1);
+        int dayOfWeek = 7 - lastMondayOfMonthBefore.until(firstOfMonth).getDays();
+        monthYearText.setText(daysInMonth+ ", "+ dayOfWeek +"," + firstOfMonth);
+
+    }
+
     private String monthYearFromDate(LocalDate date){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
         return  date.format(formatter);
